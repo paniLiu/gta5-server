f235fdbe6461f1267ab51df989140c7e236576b2
帕尼 <panilsy@icloud.com>
2021-06-17 22:25:00 +0800

    1

---
+++ resources/kwai_spawn/client.js
@@ -0,0 +1,21 @@
+const spawnPos = [686.245, 577.950, 130.461];
+
+on('onGameTypeStart', () => {
+    exports.spawnmanager.setAutoSpanCallback(() => {
+        exports.spawnmanager.spawnPlayer({
+            x: spawnPos[0],
+            y: spawnPos[1],
+            z: spawnPos[2],
+            model: 'a_m_m_skater_01',
+        }, () => {
+            emit('chat:addMessage', {
+                args: [
+                    '欢迎来到Kwai Staff 自由都市！'
+                ]
+            })
+        });
+    });
+
+    exports.spawnmanager.setAutoSpawn(true)
+    exports.spawnmanager.forceRespawn()
+})
---
+++ resources/kwai_spawn/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- This resource is part of the default Cfx.re asset pack (cfx-server-data)
+-- Altering or recreating for local use only is strongly discouraged.
+
+version '1.0.0'
+author 'Kwai GTA'
+description '主城生成点的测试程序'
+repository 'https://github.com/citizenfx/cfx-server-data'
+
+resource_type 'gametype' { name = 'Kwai Game' }
+
+client_script 'client.js'
+
+fx_version 'cerulean'
+games { 'gta5' }
