c75fa50021ad19c7f4cc852b44e7375c48dd7236
帕尼 <panilsy@icloud.com>
2021-06-18 19:41:43 +0800

    1

--- resources/[esx]/esx_atm/locales/en.lua
+++ resources/[esx]/esx_atm/locales/en.lua
@@ -1,7 +1,7 @@
 Locales['en'] = {
-  ['invalid_amount'] = '~r~That\'s an invalid amount of money~s~',
-  ['deposit_money']  = 'you have deposited ~g~$%s~s~',
-  ['withdraw_money'] = 'you have withdrawn ~g~$%s~s~',
-  ['press_e_atm']    = 'press ~INPUT_PICKUP~ for deposit or withdrawal from ~g~ATM~s~',
+  ['invalid_amount'] = '~r~系统错误，无法完成您的操作~s~',
+  ['deposit_money']  = '您往Kwai乐银行存入了 ~g~$%s~s~',
+  ['withdraw_money'] = '你从Kwai乐银行提取了 ~g~$%s~s~',
+  ['press_e_atm']    = '按下 ~INPUT_PICKUP~ 键，来进入 ~g~ATM~s~ 操作界面',
   ['atm_blip']       = 'ATM',
 }
